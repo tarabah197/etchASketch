f6e702f6b186b64274d2efc90b0483b064efa47a
tarabah197 <58168147+tarabah197@users.noreply.github.com>
2025-02-01 13:31:59 +0000

    fix: change function to creation of grid add: function to set color of chosen square

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,38 @@
 const gridContainer = document.querySelector('#grid-container');
 const containerHeight = gridContainer.clientHeight;
+const rows = 16;
+const columns = 16;
+let square;
 
-console.log(containerHeight);
-
-function createGrid (rows,columns) {
+function createGrid () {
     gridContainer.innerHTML = '';
-
-    for (i = 0; i < (rows * columns); i++){
-        const square = document.createElement('div');
+    
+    for (i = 0; i < (rows * columns); i++) {
+        square = document.createElement('div');
         square.style.width = `${Math.floor(containerHeight / rows)}px`;
         square.style.height = `${Math.floor(containerHeight / columns)}px`;
-        gridContainer.appendChild(square).className = "squares";
+        gridContainer.appendChild(square).className = "squares"; 
     }
+    // square =  document.querySelectorAll('.squares');
+    // square.forEach(div => {
+    //     div.style.borderColor = 'red';
+    // });
 } 
+
+gridContainer.onmousedown = function(event){
+    let target = event.target;
+
+    if (target.tagName != 'DIV') return;
+
+    setColor(target);
+};
+
+function setColor (targetSquare) {
+    square = targetSquare;
+    square.classList.add('squares-color');
+}
+
+
+
+window.addEventListener('load', createGrid);
+window.addEventListener('resize', createGrid);
